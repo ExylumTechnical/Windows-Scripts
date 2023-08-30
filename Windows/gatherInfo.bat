fa21d4dd2855c4ce8f838e856862caf94aac596d
Exylum Technical <94145193+ExylumTechnical@users.noreply.github.com>
2023-08-29 21:04:34 -0700

    Update gatherInfo.bat
    Dropped some additional WMIC commands that are useful for getting info about the system being used.

--- Windows/gatherInfo.bat
+++ Windows/gatherInfo.bat
@@ -1,2 +1,5 @@
-ipconfig >> info.txt
+ipconfig > info.txt
 arp -a >> info.txt
+wmic netuse get LocalName,RemoteName >> info.txt
+wmic ComputerSystem get Domain,Username,Manufacturer,Model >> info.txt
+wmic bios get serialnumber >> info.txt
