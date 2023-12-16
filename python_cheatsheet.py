9f466dcbb4bdb3959ecf9c51c6fcfb6c27243ecc
Exylum Technical <94145193+ExylumTechnical@users.noreply.github.com>
2023-12-15 17:59:37 -0800

    Create python_cheatsheet.py
    Created the python_cheetsheet.py

---
+++ python_cheatsheet.py
@@ -0,0 +1,9 @@
+#!/bin/python3.11
+
+
+# command line arguments
+import sys
+print("Number of arguments provided: "+str(len(sys.argv)))
+print("Command line options supplied:")
+for i in sys.argv:
+        print(" "+i)
