14c9794a3f8169080a45cd68770809998699d100
Exylum Technical <94145193+ExylumTechnical@users.noreply.github.com>
2023-12-15 18:22:04 -0800

    Update python_cheatsheet.py
    added more python command line argument parsing to the cheatsheet

--- python_cheatsheet.py
+++ python_cheatsheet.py
@@ -1,9 +1,30 @@
-#!/bin/python3.11
+import sys, getopt
 
-
-# command line arguments
-import sys
+### command line arguments without fancy parsing
 print("Number of arguments provided: "+str(len(sys.argv)))
 print("Command line options supplied:")
 for i in sys.argv:
         print(" "+i)
+
+### command line arguments with fancy option parsing
+def main(argv):
+        infile=""
+        outfile=""
+        try:
+                opts, args = getopt.getopt(argv, "hi:o:",["ifile=","ofile="])
+        except:
+                print("options.py -i <string> -o <string> -h")
+                sys.exit(2)
+        for opt, arg in opts:
+                if opt == '-h':
+                        print("options.py -i <string> -o <string> -h")
+                elif opt in ("-i", "--ifile"):
+                        infile=arg
+                elif opt in ("-o", "--ofile"):
+                        outfile=arg
+
+        print(infile)
+        print(outfile)
+
+if __name__== "__main__":
+        main(sys.argv[1:])
