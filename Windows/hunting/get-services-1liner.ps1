dc2da193ababbfb741989ba4ae5af27fc5aa3cb3
Nicholas Howland <94145193+ExylumTechnical@users.noreply.github.com>
2024-03-21 20:33:32 -0700

    Update and rename get-services-1liner.ps1 to get-services-1liner.ps1
    changed to directory "hunting"

---
+++ Windows/hunting/get-services-1liner.ps1
@@ -0,0 +1,6 @@
+#find and replace "path\to-output" with your desired path for file output
+ param (
+    [string]$output="C:\Users\$ENV:USERNAME\Downloads\services.csv"
+ )
+
+get-wmiobject win32_service | select name,state,displayname,processid,startmode,pathname,startname | export-csv $output -NoTypeInformation
