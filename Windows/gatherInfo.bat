faa8248ad922bec4a10d5b1edc9d363c4ff74beb
Nicholas Howland <94145193+ExylumTechnical@users.noreply.github.com>
2024-07-06 11:59:32 -0700

    Delete Windows/gatherInfo.bat

--- Windows/gatherInfo.bat
+++
@@ -1,5 +0,0 @@
-ipconfig > info.txt
-arp -a >> info.txt
-wmic netuse get LocalName,RemoteName >> info.txt
-wmic ComputerSystem get Domain,Username,Manufacturer,Model >> info.txt
-wmic bios get serialnumber >> info.txt
