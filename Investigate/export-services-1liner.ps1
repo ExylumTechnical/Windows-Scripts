758d9fb816cc62c1c81750d94d0912835916554f
Nicholas Howland <94145193+ExylumTechnical@users.noreply.github.com>
2025-02-25 14:01:44 -0800

    Update export-services-1liner.ps1
    updated csv file name

--- Investigate/export-services-1liner.ps1
+++ Investigate/export-services-1liner.ps1
@@ -1,6 +1,6 @@
 #use the -output flag to set a custom output csv file
  param (
-    [string]$output="C:\tasks.csv"
+    [string]$output="C:\services.csv"
  )
 
 get-wmiobject win32_service | select name,state,displayname,processid,startmode,pathname,startname | export-csv $output -NoTypeInformation
