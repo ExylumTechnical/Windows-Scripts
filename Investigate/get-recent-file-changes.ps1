872f51dec0a07d3298b6746a1d32c041935e7fa6
Nicholas Howland <94145193+ExylumTechnical@users.noreply.github.com>
2025-02-25 13:56:01 -0800

    Create get-recent-file-changes.ps1
    created script to dump the recent file changes of an external drive

---
+++ Investigate/get-recent-file-changes.ps1
@@ -0,0 +1,34 @@
+# this should only be used on external drives, can be time intensive if there are a lot of file changes
+# or large number of files on the specified drive
+
+param (
+    # Default to the D external drive
+    [string]$baseDirectory = "D:\",
+    [int]$hoursAgo = 0
+)
+
+# Set the directory to the user-defined one
+$targetDir = $baseDirectory
+
+# Get the current date and time
+$now = Get-Date
+
+if ($hoursAgo -gt 0) {
+    $timeThreshold = $now.AddHours(-$hoursAgo)
+    Write-Host "Files modified in the last $hoursAgo hours in $targetDir"
+    Write-Host "------------------------------------------"
+
+    # Find and list files modified within the specified hours
+    Get-ChildItem -Path $targetDir -Recurse | Where-Object { $_.LastWriteTime -ge $timeThreshold } | ForEach-Object {
+        Write-Output $_.FullName
+    }
+} else {
+    $today = $now.Date
+    Write-Host "Files modified on $today in $targetDir"
+    Write-Host "------------------------------------------"
+
+    # Find and list files modified today
+    Get-ChildItem -Path $targetDir -Recurse | Where-Object { $_.LastWriteTime.Date -eq $today } | ForEach-Object {
+        Write-Output $_.FullName
+    }
+} 
