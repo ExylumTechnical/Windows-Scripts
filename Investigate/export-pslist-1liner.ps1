5cf84648f9eff5d887f13222b76c7ff80654c741
Nicholas Howland <94145193+ExylumTechnical@users.noreply.github.com>
2025-02-25 14:02:09 -0800

    Update export-pslist-1liner.ps1
    updated file name of the exported csv

--- Investigate/export-pslist-1liner.ps1
+++ Investigate/export-pslist-1liner.ps1
@@ -1,5 +1,5 @@
 #use the -output flag to set a custom output csv file
  param (
-    [string]$output="C:\tasks.csv"
+    [string]$output="C:\pslist.csv"
  )
 Get-Wmiobject win32_process | select name,executablepath,processid,parentprocessid,commandline | export-csv $output -NoTypeInformation
